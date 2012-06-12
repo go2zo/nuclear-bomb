f1973dded760d687f3cb1957b20ae4b5a7c32f27
hanmomhanda <hanmomhanda@gmail.com>
2012-06-12 21:02:05 +0900

    ApexEditPolicyRoles 추가

---
+++ org.eclipse.papyrus.diagram.common/src/org/eclipse/papyrus/diagram/common/ApexEditPolicyRoles.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.eclipse.papyrus.diagram.common;
+
+/**
+ * @author hanmomhanda
+ *
+ */
+public interface ApexEditPolicyRoles {
+
+		/**
+		 * The key used to install a <i>Apex Custom Drag and Drop</i> EditPolicy.
+		 */
+		String CUSTOM_DRAG_DROP_ROLE = "Custom Drag and Drop"; //$NON-NLS-1$
+		
+		/**
+		 * The key used to install a <i>InteractionOperand Focus</i> EditPolicy.
+		 */
+		String INTERACTIONOPERAND_SELECTION_FOCUS_ROLE = "InteractionOperand selection focus"; //$NON-NLS-1$
+}
